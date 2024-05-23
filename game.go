d4a04953dfc35c886e1ad401b493fc0a9d84e1be
simon <simon.eickhoff@ontavio.de>
2024-05-23 11:58:39 +0200

    Clear Terminal and Print Scoreboard

--- game.go
+++ game.go
@@ -7,11 +7,22 @@ type BoardState [9]uint8
 func StartGame(playerOne Player, playerTwo Player) {
 	var boardState BoardState = initializeBoard()
 
+	clearTerminal()
+	printScoreBoard(playerOne, playerTwo)
 	printBoard(boardState, playerOne, playerTwo)
 }
 
 func initializeBoard() BoardState {
 	return BoardState{0, 0, 0, 0, 0, 0, 0, 0, 0}
+}
+
+func printScoreBoard(playerOne Player, playerTwo Player) {
+	fmt.Printf(`
+	------SCOREBOARD------
+	%v: %v
+	%v: %v
+	----------------------
+`, playerOne.name, playerOne.score, playerTwo.name, playerTwo.score)
 }
 
 func printBoard(boardState BoardState, playerOne Player, playerTwo Player) {
@@ -35,4 +46,8 @@ func printBoard(boardState BoardState, playerOne Player, playerTwo Player) {
 `, boardStateCharacters[0], boardStateCharacters[1], boardStateCharacters[2],
 		boardStateCharacters[3], boardStateCharacters[4], boardStateCharacters[5],
 		boardStateCharacters[6], boardStateCharacters[7], boardStateCharacters[8])
+}
+
+func clearTerminal() {
+	fmt.Print("\033[H\033[2J")
 }
