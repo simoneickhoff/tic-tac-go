24fd2c45f30d2a42197ad666dbbfdf46fe205f56
simon <simon.eickhoff@ontavio.de>
2024-05-23 13:08:42 +0200

    Determine current player and print it to the console

--- game.go
+++ game.go
@@ -1,19 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand/v2"
+)
 
 type BoardState [9]uint8
 
 func StartGame(playerOne Player, playerTwo Player) {
 	var boardState BoardState = initializeBoard()
 
+	getCurrentPlayerId := getCurrentPlayerIdFunc(playerOne, playerTwo)
+
+	currentPlayerId := getCurrentPlayerId()
 	clearTerminal()
 	printScoreBoard(playerOne, playerTwo)
+	printCurrentPlayer(playerOne, playerTwo, currentPlayerId)
 	printBoard(boardState, playerOne, playerTwo)
 }
 
 func initializeBoard() BoardState {
 	return BoardState{0, 0, 0, 0, 0, 0, 0, 0, 0}
+}
+
+func getCurrentPlayerIdFunc(playerOne Player, playerTwo Player) func() uint8 {
+	currentPlayerId := playerOne.id
+
+	if rand.Float64() < 0.5 {
+		currentPlayerId = playerTwo.id
+	}
+
+	getCurrentPlayer := func() uint8 {
+		if currentPlayerId == playerOne.id {
+			currentPlayerId = playerTwo.id
+		} else {
+			currentPlayerId = playerOne.id
+		}
+
+		return currentPlayerId
+	}
+
+	return getCurrentPlayer
 }
 
 func printScoreBoard(playerOne Player, playerTwo Player) {
@@ -23,6 +50,18 @@ func printScoreBoard(playerOne Player, playerTwo Player) {
 	%v: %v
 	----------------------
 `, playerOne.name, playerOne.score, playerTwo.name, playerTwo.score)
+}
+
+func printCurrentPlayer(playerOne Player, playerTwo Player, currentPlayerId uint8) {
+	currentPlayer := playerOne
+
+	if playerTwo.id == currentPlayerId {
+		currentPlayer = playerTwo
+	}
+
+	fmt.Printf(`
+	Hey %v, it's your turn! Pick a field!
+	`, currentPlayer.name)
 }
 
 func printBoard(boardState BoardState, playerOne Player, playerTwo Player) {
